117f6d5e04e3eee466f85d31634e118d1581862b
舟舟 tovarishch <70866097+kydchen@users.noreply.github.com>
2026-02-03 01:17:12 +0800

    Change data fetch duration from 30 to 260 days

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ class StockAnalysisPipeline:
             
             # 从数据源获取数据
             logger.info(f"[{code}] 开始从数据源获取数据...")
-            df, source_name = self.fetcher_manager.get_daily_data(code, days=30)
+            df, source_name = self.fetcher_manager.get_daily_data(code, days=260)
             
             if df is None or df.empty:
                 return False, "获取数据为空"
